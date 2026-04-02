7218fb1721ab8e3fd10cce04da1b698b92afd1e7
ComedyAlex <comedymitalexhd@gmail.com>
2026-04-02 03:52:16 +0200

    limited dash to 1 in air

--- PlayerController.cs
+++ PlayerController.cs
@@ -10,9 +10,13 @@ public partial class PlayerController : CharacterBody2D
 	int speed_multiplier = 30;
 	[Export]
 	public float JumpVelocity  { get; set; } = 10.0f;
-    int jump_multiplier = -30;
-	public float DashSpeed = 10.0f;
+    int jump_multiplier = -30; 
+	[Export]
+	public float WallJumpVelocity { get; set; } = 10.0f;
+	[Export]
+	public float DashVelocity { get; set; } = 10.0f;
 	int dash_multiplier = 30;
+	bool IsDashAvailable = true;
 	[Export]
 	public float Friction { get; set; } = 20;
 	[Export]
@@ -48,7 +52,7 @@ public partial class PlayerController : CharacterBody2D
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, direction.X * Speed * speed_multiplier, Acceleration);
 		}
-		else if(IsOnFloor() == false)
+		else if(!IsOnFloor())
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, AirFriction);
 		}
@@ -57,8 +61,9 @@ public partial class PlayerController : CharacterBody2D
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Friction);
 		}
 		//Dash
-		if(Input.IsActionJustPressed("dash"))
+		if(Input.IsActionJustPressed("dash") && IsDashAvailable)
 		{
+			velocity = Godot.Vector2.Zero;
 			Godot.Vector2 dashDirection = Godot.Vector2.Zero;
 			if(Input.IsActionPressed("move_up"))
 			{
@@ -92,16 +97,22 @@ public partial class PlayerController : CharacterBody2D
 			{
 				dashDirection += Godot.Vector2.Left + Godot.Vector2.Down;
 			}
-			velocity += dashDirection.Normalized() * DashSpeed * dash_multiplier;
+			velocity += dashDirection.Normalized() * DashVelocity * dash_multiplier;
+			IsDashAvailable = false;
 		}
-
-		if(Input.IsActionJustPressed("jump") && GetNode<RayCast2D>("RayCastLeft").IsColliding())
+		if(IsOnFloor())
 		{
-			velocity.Y = JumpVelocity * jump_multiplier;
+			IsDashAvailable = true;
 		}
-		if(Input.IsActionJustPressed("jump") && GetNode<RayCast2D>("RayCastRight").IsColliding())
+		if(Input.IsActionJustPressed("jump") && GetNode<RayCast2D>("RayCastLeft").IsColliding() && !IsOnFloor())
 		{
-			velocity.Y = JumpVelocity * jump_multiplier;
+			velocity.Y = WallJumpVelocity * jump_multiplier * 2;
+			velocity.X = WallJumpVelocity * -jump_multiplier;
+		}
+		if(Input.IsActionJustPressed("jump") && GetNode<RayCast2D>("RayCastRight").IsColliding() && !IsOnFloor())
+		{
+			velocity.Y = WallJumpVelocity * jump_multiplier * 2;
+			velocity.X = WallJumpVelocity * jump_multiplier;
 		}
 
 		Velocity = velocity;
